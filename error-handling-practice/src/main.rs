33a1605bb83097cead586c629bf5227134b485b7
Jbonez87 <jcastrillon087@gmail.com>
2023-03-05 22:52:33 -0500

    add backtrace and buffer overread example

--- error-handling-practice/src/main.rs
+++ error-handling-practice/src/main.rs
@@ -1,8 +1,17 @@
-mod panic_example;
+// mod panic_example;
 
-use panic_example::test_panic;
+// use panic_example::test_panic;
 
 fn main() {
-    println!("Hello, world!");
-    test_panic();
+    // test_panic();
+
+    // In this example, the panic can be found with a backtrace
+    let v = vec![1, 2, 3];
+    /* 
+     Since this index is out of the memory bounds of the Vector, 
+     this is called a buffer overread. It will attempt to retrieve
+     whatever value is at this address in memory even if it's not
+     in the Vector itself.
+    */ 
+    v[99];
 }
