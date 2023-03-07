cb4c0633e733f4adb4f90d949a6081c1091d2f35
Jbonez87 <jcastrillon087@gmail.com>
2023-03-06 23:48:04 -0500

    add recoverable_error module to mian file

--- error-handling-practice/src/main.rs
+++ error-handling-practice/src/main.rs
@@ -1,12 +1,14 @@
 // mod panic_example;
+mod recoverable_errors;
 
 // use panic_example::test_panic;
+use recoverable_errors::file_error;
 
 fn main() {
     // test_panic();
 
     // In this example, the panic can be found with a backtrace
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
     /* 
      Since this index is out of the memory bounds of the Vector, 
      this is called a buffer overread. It will attempt to retrieve
@@ -14,7 +16,9 @@ fn main() {
      in the Vector itself. This is a security issue that Rust attempts
      to prevent with a panic! macro.
     */ 
-    v[99];
+    // v[99];
 
     // In order to see the backtrace, run this command: RUST_BACKTRACE=1 cargo run
+
+    file_error();
 }
