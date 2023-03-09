5d14e99b06b20c7c1b37832a4277641c1e089430
Jbonez87 <jcastrillon087@gmail.com>
2023-03-08 21:59:18 -0500

    add alternative file_error_handling example function

--- error-handling-practice/src/recoverable_errors.rs
+++ error-handling-practice/src/recoverable_errors.rs
@@ -37,3 +37,17 @@ pub fn specific_file_error() {
     },
   };
 }
+
+pub fn alternative_file_error_handling() {
+  let greeting_file = File::open("hello_again.txt").unwrap_or_else(|error| {
+    if error.kind() == ErrorKind::NotFound {
+      File::create("hello_again.txt").unwrap_or_else(|error| {
+        panic!("Problem creating the file: {:?}", error);
+      })
+    } else {
+      panic!("Problem opening the file: {:?}", error);
+    }
+  });
+
+
+}
