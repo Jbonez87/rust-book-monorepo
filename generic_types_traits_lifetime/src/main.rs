a87d4003c5893565898b9c00a595792f6c174b94
John Castrillon <jcastrillon087@gmail.com>
2023-03-22 00:06:42 -0400

    import non_duplicate_code example

--- generic_types_traits_lifetime/src/main.rs
+++ generic_types_traits_lifetime/src/main.rs
@@ -1,6 +1,7 @@
 mod generics_examples;
 
-use generics_examples::code_duplication;
+use generics_examples::{code_duplication, non_duplicate_code};
 fn main() {
     code_duplication();
+    non_duplicate_code();
 }
