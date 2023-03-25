59a43492fab2125faa6c34d3c851bdfff79bb4dd
John Castrillon <jcastrillon087@gmail.com>
2023-03-24 23:07:08 -0400

    add largest_i32 and largest_char functions

---
+++ generic_types_traits_lifetime/src/generics_example.rs
@@ -0,0 +1,23 @@
+fn largest_i32(list: &[i32]) -> i32 {
+  let mut largest = &list[0];
+
+  for item in list {
+    if item > largest {
+      largest = item;
+    }
+  }
+
+  largest
+}
+
+fn largest_char(list: &[char]) -> char {
+  let mut largest = &list[0];
+
+  for item in list {
+    if item > largest {
+      largest = item;
+    }
+  }
+
+  largest
+}
