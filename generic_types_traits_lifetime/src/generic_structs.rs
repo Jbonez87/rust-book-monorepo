74a2f5322c8b150b1c3708b8c3e857843f27a5e9
John Castrillon <jcastrillon087@gmail.com>
2023-03-25 23:19:25 -0400

    import generic struct module and add derive debug

--- generic_types_traits_lifetime/src/generic_structs.rs
+++ generic_types_traits_lifetime/src/generic_structs.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Point<T> {
   pub x: T,
   pub y: T
--- generic_types_traits_lifetime/src/main.rs
+++ generic_types_traits_lifetime/src/main.rs
@@ -1,10 +1,15 @@
 mod function_abstraction;
 mod generics_example;
+mod generic_structs;
 
 use function_abstraction::{code_duplication, non_duplicate_code};
 use generics_example::{generics_practice};
+use generic_structs::{Point};
+
 fn main() {
     code_duplication();
     non_duplicate_code();
     generics_practice();
+    let integer = Point { x: 5, y: 4 };
+    println!("Integer is: {:?}", integer);
 }
