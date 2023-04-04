b16927c27ff41be5c36f1bf28cc9e03f6dd854aa
John Castrillon <jcastrillon087@gmail.com>
2023-04-03 22:56:26 -0500

    add summarize method to Tweet struct using Summary trait

--- generic_types_traits_lifetime/src/aggregator.rs
+++ generic_types_traits_lifetime/src/aggregator.rs
@@ -27,3 +27,9 @@ pub struct Tweet {
   pub reply: bool,
   pub retweet: bool
 }
+
+impl Summary for Tweet {
+  fn summarize(&self) -> String {
+    format!("{}: {}", self.username, self.content)
+  }
+}
