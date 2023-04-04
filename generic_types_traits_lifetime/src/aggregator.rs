a8226301f6dde1cb0882ae0346cc0303b3a76670
John Castrillon <jcastrillon087@gmail.com>
2023-04-04 08:14:03 -0500

    add comment for Summary implementation

--- generic_types_traits_lifetime/src/aggregator.rs
+++ generic_types_traits_lifetime/src/aggregator.rs
@@ -19,6 +19,10 @@ pub struct NewsArticle {
   pub content: String
 }
 
+/*
+  Note that when implementing a trait on a type, we need to 
+  put the trait after the `impl`.
+ */
 impl Summary for NewsArticle {
   fn summarize(&self) -> String {
     format!("{} by {}, ({})", self.headline, self.author, self.location)
