a46be33627e070976d617500272a64af48cdf6a2
John Castrillon <jcastrillon087@gmail.com>
2023-04-23 07:07:04 -0700

    add alternate version of longest function and comment

--- generic_types_traits_lifetime/src/lifetimes_examples.rs
+++ generic_types_traits_lifetime/src/lifetimes_examples.rs
@@ -32,3 +32,15 @@ pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     y
   }
 }
+
+/*
+  The following version of `longest` will compile since
+  lifetime 'a is in the function signature and in the `x`
+  parameter reference as well as the return type. Since it
+  returns `x`, the `y` parameter is not included in the
+  lifetime annotation.
+
+  pub fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+  }
+ */
