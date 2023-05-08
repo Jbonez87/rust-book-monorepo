4064786496f9ff596b3e9a75ba07fac6f4591b42
John Castrillon <jcastrillon087@gmail.com>
2023-05-07 21:53:53 -0400

    update comment

--- generic_types_traits_lifetime/src/first_word.rs
+++ generic_types_traits_lifetime/src/first_word.rs
@@ -3,6 +3,7 @@
   here, but it's important to understand the reason why.
   The Rust Compiler is smart enough to determine
   the lifetimes of the parameter and return type here.
+  This works because we only have one input lifetime parameter.
  */
 pub fn first_word<'a>(s: &'a str) -> &'a str {
   let bytes = s.as_bytes();
