c050e985b77f9734b61ad6ea012a518b197908cd
John Castrillon <jcastrillon087@gmail.com>
2023-05-08 21:08:17 -0400

    add integration test comment

--- adder/tests/integration_test.rs
+++ adder/tests/integration_test.rs
@@ -1,5 +1,9 @@
 use adder;
 
+/*
+  In order to execute only the integration tests, we can run
+  this command: `cargo test --test integration_test`.
+ */
 #[test]
 fn it_adds_two() {
   assert_eq!(4, adder::add_two(2));
