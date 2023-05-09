5bc20ebbab98adeda3e8d2d7dc9a3dd8da1ea90f
John Castrillon <jcastrillon087@gmail.com>
2023-05-08 22:17:31 -0400

    add common module to integration tests

--- adder/tests/integration_test.rs
+++ adder/tests/integration_test.rs
@@ -1,10 +1,13 @@
 use adder;
 
+mod common;
+
 /*
   In order to execute only the integration tests, we can run
   this command: `cargo test --test integration_test`.
  */
 #[test]
 fn it_adds_two() {
+  common::setup();
   assert_eq!(4, adder::add_two(2));
 }
