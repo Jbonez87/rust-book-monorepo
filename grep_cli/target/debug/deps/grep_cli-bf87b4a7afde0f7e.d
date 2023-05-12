d67d9e6ead8c919eafd1885d972350a993e6bfd3
John Castrillon <jcastrillon087@gmail.com>
2023-05-11 23:48:25 -0400

    import env and read command line arguments

---
+++ grep_cli/target/debug/deps/grep_cli-bf87b4a7afde0f7e.d
@@ -0,0 +1,5 @@
+/Users/jcastrillon/projects/test/rust/grep_cli/target/debug/deps/grep_cli-bf87b4a7afde0f7e.rmeta: src/main.rs
+
+/Users/jcastrillon/projects/test/rust/grep_cli/target/debug/deps/grep_cli-bf87b4a7afde0f7e.d: src/main.rs
+
+src/main.rs:
