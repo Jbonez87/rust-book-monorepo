cecec2586742c758d4d4d500d1bf7d9eaa29b142
John Castrillon <jcastrillon087@gmail.com>
2023-05-12 00:05:37 -0400

    remove target directory

--- grep_cli/target/debug/deps/grep_cli-600dd3968d81e765.d
+++
@@ -1,5 +0,0 @@
-/Users/jcastrillon/projects/test/rust/grep_cli/target/debug/deps/grep_cli-600dd3968d81e765.rmeta: src/main.rs
-
-/Users/jcastrillon/projects/test/rust/grep_cli/target/debug/deps/grep_cli-600dd3968d81e765.d: src/main.rs
-
-src/main.rs:
