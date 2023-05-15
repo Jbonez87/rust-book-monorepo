3c424e215ca0a7deff8baabd0f9d8e5a8791a8dd
John Castrillon <jcastrillon087@gmail.com>
2023-05-14 21:16:40 -0400

    add query and file_path variables from args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,5 +5,10 @@ fn main() {
         This is similar to `process.argv.slice(2)` in NodeJS.
      */
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
