01b7684da234fdcbe791bfc8c3fc10622d02c7db
John Castrillon <jcastrillon087@gmail.com>
2023-05-23 22:08:02 -0400

    add build method and change return type to Result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     /*
@@ -7,7 +8,10 @@ fn main() {
      */
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -23,15 +27,25 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // fn new(args: &[String]) -> Config {
+    //     if args.len() < 3 {
+    //         /* This is a more user friendly error message. */
+    //         panic!("Not enough arguments");
+    //     }
+    //     let query = args[1].clone();
+    //     let file_path = args[2].clone();
+
+    //     Config {query, file_path}
+    // }
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            /* This is a more user friendly error message. */
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {query, file_path}
+        Ok(Config { query, file_path })
     }
 }
 
