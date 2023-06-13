61be51f743475af06817817e19aeb511bb165eab
John Castrillon <jcastrillon087@gmail.com>
2023-06-12 22:03:48 -0400

    add ignore_case to struct

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
 pub struct Config {
   pub query: String,
@@ -16,7 +16,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { 
+          query, 
+          file_path, 
+          ignore_case 
+        })
     }
 }
 
