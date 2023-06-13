61ac69525295bf7de3a8bfb63438a36210835885
John Castrillon <jcastrillon087@gmail.com>
2023-06-12 22:12:54 -0400

    switch to eprintln! macro in main.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -20,7 +20,7 @@ fn main() {
         an error from the `run` function.
      */
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
