28e182e6bb1ac450a63c59c51d25d227e2cadd44
John Castrillon <jcastrillon087@gmail.com>
2023-06-14 23:58:21 -0400

    add gitignore to iterators project

--- iterators_and_closures/target/debug/deps/iterators_and_closures-16e8d2c3bd6de5ee.d
+++
@@ -1,5 +0,0 @@
-/Users/jcastrillon/projects/test/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-16e8d2c3bd6de5ee.rmeta: src/main.rs
-
-/Users/jcastrillon/projects/test/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-16e8d2c3bd6de5ee.d: src/main.rs
-
-src/main.rs:
--- iterators_and_closures/target/debug/deps/iterators_and_closures-9bde93e0fae9a835.d
+++
@@ -1,5 +0,0 @@
-/Users/jcastrillon/projects/test/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-9bde93e0fae9a835.rmeta: src/main.rs
-
-/Users/jcastrillon/projects/test/rust/iterators_and_closures/target/debug/deps/iterators_and_closures-9bde93e0fae9a835.d: src/main.rs
-
-src/main.rs:
