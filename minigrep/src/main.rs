955859306093d7534c95839a1b07667a2e874291
John Castrillon <jcastrillon087@gmail.com>
2023-07-21 08:22:56 -0400

    cast as types and add println to args variable in minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
         This is similar to `process.argv.slice(2)` in NodeJS.
      */
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
     
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
