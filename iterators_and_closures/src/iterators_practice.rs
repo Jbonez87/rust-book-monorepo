76a1fdf470694f98445325d21f2ef8ab8e7a6ec9
John Castrillon <jcastrillon087@gmail.com>
2023-10-19 18:19:14 -0400

    add comment instead

--- iterators_and_closures/src/iterators_practice.rs
+++ iterators_and_closures/src/iterators_practice.rs
@@ -1,7 +1,8 @@
 pub fn iterators_example() {
-  let v1 = vec![3,4,5];
-  let v1_iter = v1.iter();
-
+  /*
+    Iterators are lazy by default and don't do anything unless consumed
+    by calling one of their methods.
+   */
   let v1: Vec<i32> = vec![1,2,3];
   let v1_iter: std::slice::Iter<'_, i32> = v1.iter();
 
