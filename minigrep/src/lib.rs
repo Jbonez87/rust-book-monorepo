c5ef13c66a369b2f552acc27985b7c34e136fdb2
John Castrillon <jcastrillon087@gmail.com>
2023-11-06 08:25:01 -0500

    refactor build method in config implementation

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,6 +26,35 @@ pub struct Config {
 //     }
 // }
 
+/*
+  Alternative config implementation using the Iterator trait.
+*/
+impl Config {
+  pub fn build(
+    mut args: impl Iterator<Item = String>,
+  ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+          Some(args) => args,
+          None => return Err("Didn't get a query string.")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+  }
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
 
