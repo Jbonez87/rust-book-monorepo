74453dd0ca1efc397f5ab7f5b19a2ceb052be7ec
John Castrillon <jcastrillon087@gmail.com>
2023-11-26 14:48:55 -0500

    update types

--- art/src/main.rs
+++ art/src/main.rs
@@ -2,8 +2,8 @@ use art::PrimaryColor;
 use art::mix;
 
 fn main() {
-    let red = PrimaryColor::Red;
-    let yellow = PrimaryColor::Yellow;
+    let red: PrimaryColor = PrimaryColor::Red;
+    let yellow: PrimaryColor = PrimaryColor::Yellow;
 
     mix(red, yellow);
 }
