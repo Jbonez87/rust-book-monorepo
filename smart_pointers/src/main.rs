27700dd74eb7282f5f8f695f5c1d5310d674190b
John Castrillon <jcastrillon087@gmail.com>
2023-12-04 20:55:55 -0500

    import list enum

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,10 @@
 mod list;
 
-use list::List;
+use list::List::{Cons, Nil};
 
 fn main() {
     let b: Box<i32> = Box::new(12);
     println!("b = {}", b);
+
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
