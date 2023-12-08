04b0e5bee17bba5c85da19e3eecd1192c590dc0b
John Castrillon <jcastrillon087@gmail.com>
2023-12-07 21:42:38 -0500

    update types

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let b: Box<i32> = Box::new(12);
     println!("b = {}", b);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let list: list::List = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     println!("list is: {:?}", list);
 }
