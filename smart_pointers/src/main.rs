dfb71c0996c5d7f8cb845cbf6522d0833960c2df
John Castrillon <jcastrillon087@gmail.com>
2023-12-08 11:37:26 -0500

    add deref example

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -9,4 +9,10 @@ fn main() {
     let list: list::List = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     println!("list is: {:?}", list);
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
